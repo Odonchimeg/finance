e6a5eecff6e4b5a4a8620d4b77328ad64e2ba2f5
odnoo <odnoo861@gmail.com>
2020-10-26 00:23:27 +0800

    init function bichigdsen, program ehlehed init() duudagdaj event-uudiig holbono

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 //controller work with ui
 var uiController = (function () {
 
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn",
+        budgetValue: ".budget__value"
+    };
+
+    return {
+        getInput: function () {
+            return {
+                addType: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
+        },
+
+        setBudgetValue: function (val) {
+            document.querySelector(DOMstrings.budgetValue).textContent = "+ " + val;
+        },
+    }
+
 })();
 
 //controller work with calculation
@@ -11,27 +37,41 @@ var financeController = (function () {
 //controller work with
 var appController = (function (uiCtrl, financeCtrl) {
 
+    var DOMstrings = uiCtrl.getDOMstrings();
+
     var ctrlAddItem = function () {
 
         // 1. оруулсан өгөгдлийг дэлгэцээс авах
-        var addType = document.querySelector(".add__type").value;
-        var description = document.querySelector(".add__description").value;
-        var value = document.querySelector(".add__value").value;
+        var inputVal = uiCtrl.getInput();
 
-        console.log(addType + " === " + description + " /// " + value);
         // 2. өгөгдлийг санхүүгийн модульд дамжуулж хадгална.
+
         // 3. өгөгдлийг тохирох хэсэгт харуулна
+
         // 4. Төсвийг тооцоолно
+
         // 5. Нийт үлдэгдэл, тооцоог дэлгэцэнд харуулна
+
     }
 
-    document.querySelector(".add__btn").addEventListener("click", function () {
-        ctrlAddItem();
-    });
+    var setupEventListeners = function () {
 
-    document.addEventListener("keypress", function (event) {
-        if (event.keyCode === 13 || event.which === 13)
+        document.querySelector(DOMstrings.addBtn).addEventListener("click", function () {
             ctrlAddItem();
-    });
+        });
+
+        document.addEventListener("keypress", function (event) {
+            if (event.keyCode === 13 || event.which === 13)
+                ctrlAddItem();
+        });
+    }
+
+    return {
+        init : function(){
+            setupEventListeners();
+        }
+    }
 
 })(uiController, financeController);
+
+appController.init();
